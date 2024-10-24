e952e17b44b484352cebda05a429d992c0c7f2c6
Akshay <akshay.parate6@gmail.com>
2024-10-24 17:25:52 -0400

    Laptop : Prompt code working

--- Testing/prompt.py
+++ Testing/prompt.py
@@ -3,9 +3,20 @@ def load_model_and_tokenizer(model_name):
     model = BartForConditionalGeneration.from_pretrained(model_name)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-    data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
-    return tokenizer, model, data_collator, device
+    return tokenizer, model, device
 
+def call_action(recordedText):
+    print("Calling action")
+    if "screenshot" in recordedText:
+        img_path = action.screenshot()
+        return "You can find the screenshot at location : {}".format(img_path)
+    elif "read my screen" in recordedText:
+        text = action.read_text_from_image(action.screenshot())
+        return text
+    else:
+        context = action.fetch_from_internet(recordedText)
+        return context
+    return False
 
 def calculate_response_similarity(previous_response,current_response):
     if previous_reponse == "":
@@ -23,15 +34,23 @@ def calculate_response_similarity(previous_response,current_response):
 
 if __name__ == '__main__':
     from sky_model import generate_response
+    import actions as action
     import warnings
-    from transformers import BartForConditionalGeneration, BartTokenizer, DataCollatorForSeq2Seq
+    from transformers import BartForConditionalGeneration, BartTokenizer
     import torch
-    
     warnings.filterwarnings("ignore")
-
-    model_name = '../Training/Saved_Models/Sky/fine-tuned-bert-sentiment_2024_09_28_0'
-    tokenizer, model, data_collator, device = load_model_and_tokenizer(model_name)
+    from datetime import datetime
+    # Get the current date and time
+    now = datetime.now()
+    current_date_time = now.strftime("%Y_%m_%d")
+    model_name = '../Training/Saved_Models/Sky/fine-tuned-bert-sentiment_2024_10_04_0'
     
+    tokenizer, model, device = load_model_and_tokenizer(model_name)
+    
+    f = open("content.txt", "a")
+    f.write("\n{} Model Used : {}".format(current_date_time,model_name))
+    f.close()
+
     print("CUDA available:", torch.cuda.is_available())
     print("CUDA version:", torch.version.cuda)
     print("Device count:", torch.cuda.device_count())
@@ -42,24 +61,27 @@ if __name__ == '__main__':
     conversationList = []
     previous_reponse = ""
     while flag:
-        conversation = ""
-        recordedText = "agent_1"+input("Ask me anything : ")
-        
-        if len(conversationList) > 2:
-            conversationList = conversationList[-2:]
-        # conversation = ''.join(conversationList)+" agent_1:"+recordedText
+        # conversation = ""
+        # recordedText = "agent_1"+input("Ask me anything : ")
+        recordedText = input("Ask me anything : ")
+        f = open("content.txt", "a")
+        f.write("\n{} Recorded Text: {}".format(current_date_time,recordedText))
+        action_response = call_action(recordedText)
+        if not action_response:
+            continue
         print(recordedText)
-        print()
-        response = generate_response(recordedText, tokenizer, device, model)
-        # similarity_score = calculate_response_similarity(previous_reponse,response)
-        # if similarity_score > 0.5:
-        #     response = generate_response("agent_1:"+recordedText, tokenizer, device, model)
-        # previous_reponse = response.split(",<Emotion>:")[0].split("<Content>:")[1]
-        # conversation = conversation+" agent_2:"+response.split(",<Emotion>:")[0].split("<Content>:")[1]
-        # conversationList.append("agent_1:"+recordedText+" agent_2:"+response.split(",<Emotion>:")[0].split("<Content>:")[1])
-        # print(response.split(",<Emotion>:")[0].split("<Content>:")[1])
+        final_query = "<context>{}agent_1:{}".format(action_response["documents"][0][0],recordedText)
+        # response = generate_response("<summary>"+action_response, tokenizer, device, model)
+        response = generate_response(final_query, tokenizer, device, model)
         print(response)
-        # print(conversation)
-        # print(conversationList)
+        try:
+            f = open("content.txt", "a")
+            f.write("\n{} Context(Action): {}".format(current_date_time,action_response))
+            f.write("\n{} Model Response: {}".format(current_date_time,response))
+            f.close()
+        except UnicodeEncodeError as e:
+            print(e)
+
+
 # Optionally, release GPU memory
     torch.cuda.empty_cache()
